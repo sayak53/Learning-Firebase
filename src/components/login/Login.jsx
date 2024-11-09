b9111598a4eeecf708cf8dc5529e9c9087a410fd
sayak53 <sayakchakraborty928@gmail.com>
2024-11-09 13:32:35 +0530

    learned how to login with google

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,20 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase/firebase.init";
 
 const Login = () => {
+    const provider = new GoogleAuthProvider();
+    const handleGoogleSignin = () => {
+        signInWithPopup(auth,provider)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
     return (
         <div>
-           <button>log in with google</button> 
+           <button onClick={handleGoogleSignin}>log in with google</button> 
         </div>
     );
 };
