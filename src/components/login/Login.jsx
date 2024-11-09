5d80cedad51f01d8d2a3c9cdb6bb370c7bc14350
sayak53 <sayakchakraborty928@gmail.com>
2024-11-09 14:22:11 +0530

    added login and sign out options

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,41 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const[user,setUser] = useState(null)
     const provider = new GoogleAuthProvider();
     const handleGoogleSignin = () => {
         signInWithPopup(auth,provider)
         .then(result => {
             console.log(result)
+            setUser(result.user)
         })
         .catch(error => {
             console.log(error)
+            setUser(null)
         })
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(() => {
+            setUser(null)
+        })
+        .catch(error => console.log(error))
     }
     return (
         <div>
-           <button onClick={handleGoogleSignin}>log in with google</button> 
+           {
+            user?<button onClick={handleSignOut}>Sign Out</button>:<button onClick={handleGoogleSignin}>log in with google</button>
+           }
+           { 
+             
+               user && <div>
+               <h4>{user.displayName}</h4>
+               <p>{user.email}</p>
+               <img src="https://lh3.googleusercontent.com/a/ACg8ocISw2JNu-UpVX6BXKVofn-z6vu8Ak7Ieshr0a46AT9Q9WZFE2Fw=s96-c" alt="profile pic" />
+            </div>
+           }
         </div>
     );
 };
