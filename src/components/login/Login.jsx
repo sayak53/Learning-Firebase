ebf1f0b41009c72bff88855c4f8d53eebb6549d5
sayak53 <sayakchakraborty928@gmail.com>
2024-11-11 13:12:39 +0530

    added some styles for login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,41 +3,42 @@ import auth from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
-    const[user,setUser] = useState(null)
-    const provider = new GoogleAuthProvider();
-    const handleGoogleSignin = () => {
-        signInWithPopup(auth,provider)
-        .then(result => {
-            console.log(result)
-            setUser(result.user)
-        })
-        .catch(error => {
-            console.log(error)
-            setUser(null)
-        })
-    }
-    const handleSignOut = () => {
-        signOut(auth)
-        .then(() => {
-            setUser(null)
-        })
-        .catch(error => console.log(error))
-    }
-    return (
+  const [user, setUser] = useState(null);
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+        setUser(null);
+      });
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignin}>log in with google</button>
+      )}
+      {user && (
         <div>
-           {
-            user?<button onClick={handleSignOut}>Sign Out</button>:<button onClick={handleGoogleSignin}>log in with google</button>
-           }
-           { 
-             
-               user && <div>
-               <h4>{user.displayName}</h4>
-               <p>{user.email}</p>
-               <img src="https://lh3.googleusercontent.com/a/ACg8ocISw2JNu-UpVX6BXKVofn-z6vu8Ak7Ieshr0a46AT9Q9WZFE2Fw=s96-c" alt="profile pic" />
-            </div>
-           }
+          <h4>{user.displayName}</h4>
+          <p>{user.email}</p>
+          <img src={user.photoURL} alt="profile pic" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
